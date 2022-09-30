adb70a0cf41714649522fb3a4906cb4d98356487
JonWong2020 <jonathannwong50@aol.com>
2022-09-30 10:40:19 -0700

    added book model functions

---
+++ models/Book.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Book extends Model {}
+
+Book.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+        review_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'review',
+                key: 'id',
+            },
+        },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'book',
+    }
+);
+
+module.exports = Book;
