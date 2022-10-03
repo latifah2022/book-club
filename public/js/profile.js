8522bdf16cb2347ba143741ef84ed15c1fcb4836
Latifah Zawedde <zawedde2021@gmail.com>
2022-10-03 11:45:19 -0700

    updating js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,11 +1,11 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const name = document.querySelector('#book-name').value.trim();
+  const needed_review = document.querySelector('#book-reviews').value.trim();
+  const description = document.querySelector('#book-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && needed_review && description) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
       body: JSON.stringify({ name, needed_review, description }),
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create book');
+      alert('Failed to create');
     }
   }
 };
