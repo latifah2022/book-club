7e8376a4268b635840f3dd0a1a7d8dc3480df219
maiachargois <maiachargois@Maias-Air.lan>
2022-10-03 11:45:22 -0700

    added the s

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
-// adding file so that it will come through github
 const router = require('express').Router();
 const userRoutes = require('./userRoute');
 const reviewRoutes = require('./reviewRoute');
 
-router.use('/user', userRoute);
-router.use('/review', reviewRoute);
+router.use('/user', userRoutes);
+router.use('/review', reviewRoutes);
 
 module.exports = router;
